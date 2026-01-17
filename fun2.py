dc7fd36a37caf6c1e4e9ca80d00999d76d865b7d
utkarsh shukla <u9819787@gmail.com>
2026-01-17 09:41:40 +0530

    function

---
+++ fun2.py
@@ -0,0 +1,12 @@
+namr="utkarsh"
+def name (namr):
+    print(namr)
+name(namr)
+a = int (input("enter the value"))
+b = int (input("enter value"))
+def add (x,y):
+    print ("addition wil be", a+b)
+add (a,b)
+def sq(a):
+    print(a*a)
+sq(a)
