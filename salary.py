22441fb3ed44367cd489a6343e79cc7df0d1f4db
utkarsh shukla <shuklautkarsh772@gmail.com>
2026-01-17 10:23:44 +0530

    object

---
+++ salary.py
@@ -0,0 +1,9 @@
+class salary:
+    def __init__ (self,name , salary):
+        self.name= name
+        self.salary = salary
+
+c1= salary("veer",18000000)
+c2 = salary ("garg", 12000000)
+print (c1.name, c1.salary)
+print (c2.name, c2.salary)
