afb02d6964ba630c0b36166e23d9936ebb6b78ee
utkarsh shukla <shuklautkarsh772@gmail.com>
2026-01-21 09:51:54 +0530

    math module

---
+++ math.py
@@ -0,0 +1,34 @@
+import math
+a = 645
+b =math.sqrt(a)
+print (b)
+c = 6
+d  =  math.factorial(c)
+print (d)
+e = 4.7
+f = math.floor(e)
+print (f)
+g = math.ceil(e)
+print (g)
+h = 5
+i = 3
+j = h.__pow__(i)
+print(j)
+k = -18
+l = math.fabs(k)
+print(l)
+m = 90
+n = math.sin(m)
+o = math.cos(m)
+p = math.tan(m)
+q= math.radians(m)
+print (m)
+print(n)
+print(o)
+print(p)
+print (q)
+r= 36
+s= 60
+t = math.gcd(r,s)
+print(t)
+v = print(math.e.__pow__(2))
